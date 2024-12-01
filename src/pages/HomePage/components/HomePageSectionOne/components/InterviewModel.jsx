50757c4f961dfd250fe77e0dc7c3b3c34504c85e
David Tolmay <davidt@missionreadyhq.com>
2024-12-02 09:23:34 +1300

    setup backend repo

---
+++ src/pages/HomePage/components/HomePageSectionOne/components/InterviewModel.jsx
@@ -0,0 +1,11 @@
+import styles from "../../../../../styles/HomePageInterviewModel.module.css";
+
+function InterviewModel() {
+  return (
+    <div>
+      <div>AI Interview Model</div>
+    </div>
+  );
+}
+
+export default InterviewModel;
